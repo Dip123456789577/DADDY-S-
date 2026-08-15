ac7ba90ae1e0398c171531c58bea6f3ea12eb2b8
Diptanshu <diptanshudas920@gmail.com>
2026-08-15 11:36:36 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   plugins: [
-    tanstackStart({
-      server: {
-        preset: "vercel",
-      },
-    }),
+    tanstackStart(),
     react(),
     tailwindcss(),
   ],
